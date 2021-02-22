c4bdd40ffd884dd2db9140f3d63a37d84897a04b
marjanajenia <63357966+marjanajenia@users.noreply.github.com>
2021-02-23 00:22:20 +0600

    done

--- second.php
+++ second.php
@@ -7,53 +7,40 @@
 	<h1>Registration Form</h1>
 	<?php 
 
-		$FirstName = $LastName = "";
-		#$FirstNameErr = $LastNameErr = "";
+	
 
 		if($_SERVER['REQUEST_METHOD'] == "POST") {
 
-			if(empty($_POST['fname'])) {
-				echo"Please fill up the firstname";
-				echo "<br>";
+			if(empty($_POST['fname']) && empty($_POST['lname']) && empty($_POST['male']) && empty($_POST['female']) && empty($_POST['email']) && empty($_POST['username']) && empty($_POST['pass']) && empty($_POST['recoveryemail']))
+			 {
+				echo"Please fill up the the form";
+	
 			}
 			else {
 				$FirstName = $_POST['fname'];
-			}
-
-			if(empty($_POST['lname'])) {
-				 echo "Please fill up the lastname";
-				 echo "<br>";
-			}
-			else {
 
 				$lastName = $_POST['lname'];
+
+				$gender = $_POST['gender'];
+
+				$email = $_POST['email'];
+				$username=$_POST['username'];
+				$pass=$_POST['pass'];
+				$recoveryemial=$_POST['recoveryemail'];
 				
-			}
-
-			if(empty($_POST['gender'])) {
-				 echo "Please fill up the gender";
-				 echo "<br>";
-			}
-			else {
-
-				$lastName = $_POST['gender'];
 				
-			}
+			
 
-			if(empty($_POST['email'])) {
-				 echo "Please fill up the email";
-				 echo "<br>";
-			}
-			else {
-
-				$lastName = $_POST['lname'];
-				
-			}
+			$f=fopen("save.txt","a");
+			fwrite($f,$FirstName. "" .$lastName. "" .$gender. "" .$email. "" .$username. "" .$pass. "" .$recoveryemial. "\n");
+			fclose($f);
+		      }
+		  }
 
 			
 
 			
-		}
+		
 	?>
 
 </body>
